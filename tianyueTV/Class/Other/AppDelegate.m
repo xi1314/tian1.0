c36e588592062857b02e96ca5a82db4352231cad
PowerCG <mac-chen@Mac-chendeMac-mini.local>
2017-04-07 12:04:11 +0800

    修改推送代码

--- tianyueTV/Class/Other/AppDelegate.m
+++ tianyueTV/Class/Other/AppDelegate.m
@@ -32,7 +32,6 @@
 //#import "PLMediaStreamingKit.h"
 #import "GuideViewController.h"
 
-
 #import "TalkingData.h"
 #import "TalkingDataSMS.h"
 
@@ -267,37 +266,50 @@
 }
 
 
+
 #pragma mark- JPUSHRegisterDelegate
 
 // iOS 10 Support
 - (void)jpushNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(NSInteger))completionHandler {
+
+    /*
+     NSDictionary *aps = userInfo[@"aps"];
+     NSString *alert = aps[@"alert"];
+     */
+    
     // Required
     NSDictionary * userInfo = notification.request.content.userInfo;
+    
     if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
         [JPUSHService handleRemoteNotification:userInfo];
     }
-    completionHandler(UNNotificationPresentationOptionAlert); // 需要执行这个方法，选择是否提醒用户，有Badge、Sound、Alert三种类型可以选择设置
+    
+    // 需要执行这个方法，选择是否提醒用户，有Badge、Sound、Alert三种类型可以选择设置
+    completionHandler(UNNotificationPresentationOptionAlert);
 }
 
 // iOS 10 Support
 - (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)())completionHandler {
     // Required
     NSDictionary * userInfo = response.notification.request.content.userInfo;
+    
     if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
         [JPUSHService handleRemoteNotification:userInfo];
     }
-    completionHandler();  // 系统要求执行这个方法
+    
+    // 系统要求执行这个方法
+    completionHandler();
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
-    
+
     // Required, iOS 7 Support
     [JPUSHService handleRemoteNotification:userInfo];
     completionHandler(UIBackgroundFetchResultNewData);
 }
 
 - (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    
+
     // Required,For systems with less than or equal to iOS6
     [JPUSHService handleRemoteNotification:userInfo];
 }
