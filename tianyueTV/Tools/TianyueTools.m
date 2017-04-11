bb168fcad56e813652fb8d6fe4ff50e90082725a
PowerCG <mac-chen@Mac-chendeMac-mini.local>
2017-04-11 11:38:46 +0800

    修改电话号码判断

--- tianyueTV/Tools/TianyueTools.m
+++ tianyueTV/Tools/TianyueTools.m
@@ -12,67 +12,25 @@
 
 
 /**
- 电话号码判断
+ 电话号码判断(包括手机和座机)
 
  @param mobileNum 输入的号码
  @return 是否为正确的电话号码
  */
 + (BOOL)isMobileNumber:(NSString *)mobileNum
 {
+    // 判断手机
+    NSString *phoneRegex = @"^((13[0-9])|(15[^4,\\D])|(18[0,0-9]))\\d{8}$";
     
-    /**
-     
-     * 手机号码
-     
-     * 移动：134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188
-     
-     * 联通：130,131,132,152,155,156,185,186
-     
-     * 电信：133,1349,153,180,189
-     
-     */
-    
-    NSString * MOBILE = @"^1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$";
-    
-    /**
-     
-     * 中国移动：China Mobile
-     
-     * 134[0-8],135,136,137,138,139,150,151,157,158,159,182,187,188
-     
-     */
-    
-    NSString * CM = @"^1(34[0-8]|(3[5-9]|5[017-9]|8[278])\\d)\\d{7}$";
-    
-    /**
-     
-     * 中国联通：China Unicom
-     
-     * 130,131,132,152,155,156,185,186
-     
-     */
-    
-    NSString * CU = @"^1(3[0-2]|5[256]|8[56])\\d{8}$";
-    
-    /** 中国电信：China Telecom   133,1349,153,180,189 */
-    
-    NSString * CT = @"^1((33|53|8[09])[0-9]|349)\\d{7}$";
-    
+    NSPredicate *phoneTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",phoneRegex];
+
     /** 大陆地区固话及小灵通  区号：010,020,021,022,023,024,025,027,028,029 号码：七位或八位 */
     
     NSString * PHS = @"^0(10|2[0-5789]|\\d{3})\\d{7,8}$";
     
-    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
-    
-    NSPredicate *regextestcm = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM];
-    
-    NSPredicate *regextestcu = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU];
-    
-    NSPredicate *regextestct = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT];
-    
     NSPredicate *regextestphs = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", PHS];
     
-    if (([regextestmobile evaluateWithObject:mobileNum] == YES) || ([regextestcm evaluateWithObject:mobileNum] == YES) || ([regextestct evaluateWithObject:mobileNum] == YES) || ([regextestcu evaluateWithObject:mobileNum] == YES) || ([regextestphs evaluateWithObject:mobileNum] == YES)) {
+    if (([phoneTest evaluateWithObject:mobileNum] == YES) || ([regextestphs evaluateWithObject:mobileNum] == YES)) {
         
         return YES;
         
